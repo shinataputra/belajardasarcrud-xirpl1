b793e45f2c2df34c05a1714f2aa38cb31f9feeb8
shinataputra <71615861+shinataputra@users.noreply.github.com>
2025-08-19 11:39:51 +0700

    fix : update pada prosesinput redirect ke index setelah kirim data

--- index.php
+++ index.php
@@ -7,19 +7,9 @@
   <title>Document</title>
 </head>
 
-<!-- koneksi database -->
-<?php
-include 'config.php';
-?>
-
-<!-- proses input data -->
-<?php
-include 'prosesinput.php';
-?>
-
 <body>
   <!-- form  -->
-  <form action="" method="post">
+  <form action="prosesinput.php" method="post">
     <input type="text" name="nama" placeholder="Masukkan Nama Anda" required>
     <input type="text" name="kelas" placeholder="Masukkan Kelas Anda" required>
     <input type="submit" value="Kirim Data">
--- prosesinput.php
+++ prosesinput.php
@@ -1,19 +1,27 @@
+<!-- koneksi database -->
+<?php
+include 'config.php';
+?>
+
+
 <!-- menyimpan inputan -->
 <?php
 // get data dari form
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $nama = $_POST['nama'];
-  $kelas = $_POST['kelas'];
+    $nama = $_POST['nama'];
+    $kelas = $_POST['kelas'];
 
-  // insert data ke database
-  $sql = "INSERT INTO tbsiswa (nama, kelas) VALUES ('$nama', '$kelas')";
-  $result = mysqli_query($conn, $sql);
+    // insert data ke database
+    $sql = "INSERT INTO tbsiswa (nama, kelas) VALUES ('$nama', '$kelas')";
+    $result = mysqli_query($conn, $sql);
 
-  // cek apakah data berhasil disimpan
-  if ($result) {
-    echo "Data berhasil disimpan";
-  } else {
-    echo "Data gagal disimpan: " . mysqli_error($conn, $sql);
-  }
+    // cek apakah data berhasil disimpan
+    if ($result) {
+        // redirect ke halaman index.php
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Data gagal disimpan: " . mysqli_error($conn, $sql);
+    }
 }
 ?>
