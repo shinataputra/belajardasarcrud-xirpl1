5417d4a72033f5b2059bbaff92dfb6c8e915623c
shinataputra <71615861+shinataputra@users.noreply.github.com>
2026-01-06 13:37:39 +0700

    udpdate : config

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <!-- koneksi database -->
 <?php
-$conn = mysqli_connect("localhost", "root", "root", "dbsiswa");
+
+$conn = mysqli_connect("localhost", "root", "", "dbsiswa");
 // cek koneksi
 if ($conn->connect_error) {
   die("Koneksi gagal: " . $conn->connect_error);
